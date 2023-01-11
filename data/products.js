ecaef37598aee83e6dc4b5b16745dbf01b0ada52
algol007 <adhyformerz@gmail.com>
2023-01-11 15:25:02 +0800

    initial commit

---
+++ data/products.js
@@ -0,0 +1,54 @@
+const products = [
+  {
+    _id: "1",
+    name: "Velcro Ballerinas For Girls (Pink)",
+    image: "/images/6.png",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    price: 1000000,
+    countInStock: 3, 
+    rating: 4,
+    numReviews: 10
+  },
+  {
+    _id: "2",
+    name: "Velcro Ballerinas For Girls (Pink)",
+    image: "/images/6.png",
+    description: "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    price: 2000000,
+    countInStock: 4, 
+    rating: 5,
+    numReviews: 11
+  },
+  {
+    _id: "3",
+    name: "Velcro Ballerinas For Girls (Pink)",
+    image: "/images/6.png",
+    description: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
+    price: 3000000,
+    countInStock: 5, 
+    rating: 6,
+    numReviews: 12
+  },
+  {
+    _id: "4",
+    name: "Velcro Ballerinas For Girls (Pink)",
+    image: "/images/6.png",
+    description: "Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of de Finibus Bonorum et Malorum (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum.",
+    price: 4000000,
+    countInStock: 6, 
+    rating: 7,
+    numReviews: 13
+  },
+  {
+    _id: "5",
+    name: "Velcro Ballerinas For Girls (Pink)",
+    image: "/images/6.png",
+    description: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.",
+    price: 5000000,
+    countInStock: 7, 
+    rating: 8,
+    numReviews: 14
+  },
+]
+
+export default products;
